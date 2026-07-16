858ae031dcd9348bfe698e330a2a29df8d0bd388
Abishuuu <sriabisan06@gmail.com>
2026-07-16 23:22:48 +0530

    Update the activity_profile.xml with correct drawables and material card

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -33,6 +33,7 @@ dependencies {
     implementation("androidx.appcompat:appcompat:1.7.1")
     implementation("com.google.android.material:material:1.12.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+    implementation("de.hdodenhof:circleimageview:3.1.0")
 
     implementation("com.hbb20:ccp:2.7.3")
 
